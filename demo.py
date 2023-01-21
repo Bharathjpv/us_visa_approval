fc4c4674d6eb265c5eec269fd1e3c2e425fad5b2
Bharathjpv <bharath@ineuron.ai>
2023-01-21 12:08:30 +0530

    data ingestion completed

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
-from us_visa.configuration.mongo_db_connection import MongoDBClient
+# from us_visa.configuration.mongo_db_connection import MongoDBClient
 
-ins = MongoDBClient()
+# ins = MongoDBClient()
+
+from us_visa.entity.config_entity import DataIngestionConfig
+from us_visa.entity.artifact_entity import DataIngestionArtifact
+
+from us_visa.components.data_ingestion import DataIngestion
+
+di_ins = DataIngestion(DataIngestionConfig)
+
+di_art = di_ins.initiate_data_ingestion()
