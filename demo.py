435e6066abe64449f2c723a0df6ca6ef2d23ca9c
Bharathjpv <bharath@ineuron.ai>
2023-01-21 10:48:30 +0530

    MongoDB connection completed

---
+++ demo.py
@@ -0,0 +1,3 @@
+from us_visa.configuration.mongo_db_connection import MongoDBClient
+
+ins = MongoDBClient()
--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -5,14 +5,16 @@ from from_root import from_root
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(from_root(), "logs", LOG_FILE)
 
-os.makedirs(logs_path, exist_ok=True)
+log_dir = 'logs'
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
+
+os.makedirs(log_dir, exist_ok=True)
+
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=logs_path,
     format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
     level=logging.DEBUG,
 )
