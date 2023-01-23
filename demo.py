5a059a2d79a09d8d5d421fe141359a314374800b
Bharathjpv <bharath@ineuron.ai>
2023-01-23 12:57:13 +0530

    data validation completed

--- demo.py
+++ demo.py
@@ -2,11 +2,16 @@
 
 # ins = MongoDBClient()
 
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.entity.artifact_entity import DataIngestionArtifact
+from us_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 from us_visa.components.data_ingestion import DataIngestion
+from us_visa.components.data_validation import DataValidation
 
 di_ins = DataIngestion(DataIngestionConfig)
 
 di_art = di_ins.initiate_data_ingestion()
+
+dv_ins = DataValidation(data_ingestion_artifact=di_art, data_validation_config=DataValidationConfig)
+
+dv_art = dv_ins.initiate_data_validation()
--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 DATABASE_NAME = "US_VISA"
 
 COLLECTION_NAME = "visa_data"
@@ -10,6 +12,7 @@ ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "usvisa.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -19,3 +22,10 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
