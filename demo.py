308e926423200999fdd347c8c39c26094a15c67a
Bharathjpv <bharath@ineuron.ai>
2023-01-24 13:52:37 +0530

    model evaluation completed

--- demo.py
+++ demo.py
@@ -2,12 +2,17 @@
 
 # ins = MongoDBClient()
 
-from us_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+# from us_visa.configuration.aws_connection import S3Client
+
+# ins = S3Client()
+
+from us_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
 from us_visa.components.model_trainer import ModelTrainer
+from us_visa.components.model_evaluation import ModelEvaluation
 
 di_ins = DataIngestion(DataIngestionConfig)
 
@@ -24,3 +29,7 @@ dt_art = dt_ins.initiate_data_transformation()
 mt_ins = ModelTrainer(data_transformation_artifact=dt_art, model_trainer_config=ModelTrainerConfig)
 
 mt_art = mt_ins.initiate_model_trainer()
+
+me_ins = ModelEvaluation(model_eval_config=ModelEvaluationConfig, data_ingestion_artifact=di_art, model_trainer_artifact=mt_art)
+
+me_art = me_ins.initiate_model_evaluation()
