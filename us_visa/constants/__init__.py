631cfa73028926537f3194c3612daaf11925b90e
Bharathjpv <bharath@ineuron.ai>
2023-01-25 17:56:48 +0530

    application

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -60,3 +60,6 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
 MODEL_BUCKET_NAME = "usvisa-model"
 MODEL_PUSHER_S3_KEY = "model-registry"
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
